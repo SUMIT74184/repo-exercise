cee126c92b65416815c1f3653902b2a45bd63e49
sumit rai <rais74184@gmail.com>
2023-12-26 14:46:28 +0530

    added new javafunction

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 testing and requirement
 author sumit rai
 
+learnig is goin on brothers
---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum{
+
+    public void classy(int a){
+        System.out.println("sumits file");
+    }
+    public static void main(String []args){
+        System.out.println("my new java program");
+        // man c=new man();
+        // classy.c();
+
+    }
+}
